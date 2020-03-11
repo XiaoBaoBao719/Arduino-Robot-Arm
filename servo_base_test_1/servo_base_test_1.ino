73eb3ea288949401d73901d18191691a2d6fd4ed
Xiao Bao Bao <xiao-bao719@hotmail.com>
2020-03-11 16:09:16 -0700

    Update servo_base_test_1.ino
    rotation testing

--- servo_base_test_1/servo_base_test_1.ino
+++ servo_base_test_1/servo_base_test_1.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
+#include <math.h>
 
 Servo servo_base;
 Servo servo_shoulder;
@@ -7,18 +8,28 @@ Servo servo_arm1;
 Servo servo_arm2;
 Servo servo_arm3;
 
+int rotStart, rotEnd, counter, rotRate;
+
 void setup() {
   // put your setup code here, to run once:
-  servo_base.attach(3);
-  servo_shoulder.attach(5);
-  servo_arm1.attach(6);
+  servo_base.attach(9);
+  servo_shoulder.attach(6);
+  servo_arm1.attach(5);
+  servo_arm2.attach(3);
+
+  //rotation smoothing test init
+  rotStart = 0;
+  rotEnd = 180;
+  rotRate = 2; //1 degree per 100 ms
 
   delay(1000);
   servo_base.write(90);
   //delay(5000);
-  servo_shoulder.write(120);
+  servo_shoulder.write(90);
   //delay(5000);
-  servo_arm1.write(180);
+  servo_arm1.write(90);
+  //delay(5000);
+  servo_arm2.write(90);
   //delay(10000);
   /*
   servo_base.write(0);
@@ -65,9 +76,19 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //servo_shoulder.write(110);
-  //delay(5000);
-  //servo_shoulder.write(80);
-  //delay(5000);
+
+  //rotation smoothing test 
+   for(counter = rotStart; counter < rotEnd; counter += rotRate)
+  {
+    //increment servo by 1 degree
+    servo_arm1.write(fabs(counter));
+    //servo_shoulder.write(fabs(counter));
+    //servo_base.write(fabs(counter));
+    delay(100);
+  }
+  //flips initial and final rotation bounds
+  int temp = rotStart;
+  rotStart = rotEnd * -1;
+  rotEnd = temp;
+  
 }
